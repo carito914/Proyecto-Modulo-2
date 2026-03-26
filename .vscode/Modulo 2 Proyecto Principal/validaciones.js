85ff0faddeb5f792be0c1895080d2ae3e138fbf1
Carolina Olmos <carolinacortes@gmail.com>
2026-03-26 14:35:25 -0300

    Primer commit - Proyecto FullStack ordenado

---
+++ .vscode/Modulo 2 Proyecto Principal/validaciones.js
@@ -0,0 +1,21 @@
+console.log("javascript funcionando");
+
+
+
+
+
+
+
+function validarCorreo(correo){
+    return validarCorreo.includes(@)&& correo.includes("")
+
+}
+
+function valodarFormulario(){
+    const nombre= getElementById("nombre");
+    const correo=getElementById("correo");
+    const mensaje=getElementById("mensaje");
+
+}
+
+if
